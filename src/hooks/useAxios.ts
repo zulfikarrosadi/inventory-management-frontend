14c7e5e811fa9e5775bf5ddbaff3479999816f4f
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2024-04-20 16:14:37 +0700

    attach response interceptor to axios instance

---
+++ src/hooks/useAxios.ts
@@ -0,0 +1,34 @@
+import { useEffect } from 'react';
+import axios from '../axios';
+import useRefreshToken from './useRefreshToken';
+
+function useAxios() {
+  const refresh = useRefreshToken();
+
+  useEffect(() => {
+    const responseInterceptor = axios.interceptors.response.use(
+      function (config) {
+        return config;
+      },
+      async function (error) {
+        if (error.response && error.response.status === 401) {
+          try {
+            await refresh();
+            return axios.request(error.config);
+          } catch (error) {
+            console.log('respose_interceptor', error);
+            return Promise.reject(error);
+          }
+        }
+      },
+    );
+
+    return () => {
+      axios.interceptors.response.eject(responseInterceptor);
+    };
+  }, []);
+
+  return axios;
+}
+
+export default useAxios;
